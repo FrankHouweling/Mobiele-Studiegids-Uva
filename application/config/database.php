2d0a2310d6ecd12b168307dc34f1888dda9d1c72
Maurice <maurice.stam@student.uva.nl>
2012-03-04 05:10:40 -0500

    database credentials added in config/database.php

--- application/config/database.php
+++ application/config/database.php
@@ -49,9 +49,9 @@ $active_group = 'default';
 $active_record = TRUE;
 
 $db['default']['hostname'] = 'localhost';
-$db['default']['username'] = '';
-$db['default']['password'] = '';
-$db['default']['database'] = '';
+$db['default']['username'] = 'jharvard';
+$db['default']['password'] = 'crimson';
+$db['default']['database'] = 'jharvard_lab1';
 $db['default']['dbdriver'] = 'mysql';
 $db['default']['dbprefix'] = '';
 $db['default']['pconnect'] = TRUE;
--- application/models/faculteiten.php
+++ application/models/faculteiten.php
@@ -3,6 +3,12 @@
 
 class Faculteiten extends CI_Model {
 
+     public function __construct()
+    {
+        // Call the Model constructor
+        parent::__construct();
+    }
+
 
     public function get_faculteiten()
     {
