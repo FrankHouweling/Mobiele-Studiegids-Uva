3c85606f12104aeaa7c1e98c85286229655df02c
Frank Houweling <houweling.frank@gmail.com>
2012-03-08 20:13:25 +0100

    Nu werkt alle studies echt :)

--- application/views/allestudies.php
+++ application/views/allestudies.php
@@ -4,7 +4,7 @@
 			<ul data-role="listview" data-inset="true" data-filter="true" >
                      <?php foreach($programName as $studie): ?>
 
-                    <li><a href="#"><?=$studie['programName']?></a></li>
+                    <li><a href="../../ziestudie/studie/<?=$studie['id']?>"><?=$studie['programName']?></a></li>
 
                     <?php endforeach; ?>
             </ul>
