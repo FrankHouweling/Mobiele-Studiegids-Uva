8872615e4d751c46e0da87d21cac4dafc2039bbb
Frank Houweling <houweling.frank@gmail.com>
2012-03-08 22:23:43 +0100

    Small changes

--- application/views/ziestudie.php
+++ application/views/ziestudie.php
@@ -57,10 +57,10 @@
 			Programmatype: <u><?php echo $programType?></u>.
 		</li>
 		<li>
-			Eersvolgende instroommogelijkheid: <?php echo $startingYear ?>.
+			Eersvolgende instroommogelijkheid: <u><?php echo $startingYear ?></u>.
 		</li>
 		<li>
-			Dit vak wordt vooral in het <?php echo $instructionLanguageFull?>.
+			Dit vak wordt vooral in het <u><?php echo $instructionLanguageFull?></u>.
 		</li>
 	</ul>
 	
