25be7d4756938ce1c81c8af6e391ce3633af7d03
Frank Houweling <houweling.frank@gmail.com>
2012-03-16 17:11:09 +0100

    And again a small fix in the path's

--- application/views/faculteiten.php
+++ application/views/faculteiten.php
@@ -4,7 +4,7 @@
 			
                     <?php foreach($faculty_name as $faculteit): ?>
 
-                    <li><a href="faculteit/<?=$faculteit['id']?>/"><?=$faculteit['fullFaculty']?></a></li>
+                    <li><a href="http://project0/index.php/faculteit/<?=$faculteit['id']?>"><?=$faculteit['fullFaculty']?></a></li>
 
                     <?php endforeach; ?>
                     
